e2807200cf1288ed984e8bbcd82b82011b45c09c
Russian Rebouças <russian.reboucas@gmail.com>
2017-11-11 21:49:41 -0300

    chore: align imports

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
-const path = require('path');
-const favicon = require('serve-favicon');
-const compress = require('compression');
-const cors = require('cors');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
+const path           = require('path');
+const favicon        = require('serve-favicon');
+const compress       = require('compression');
+const cors           = require('cors');
+const helmet         = require('helmet');
+const bodyParser     = require('body-parser');
 
-const feathers = require('feathers');
-const configuration = require('feathers-configuration');
-const hooks = require('feathers-hooks');
-const rest = require('feathers-rest');
-const socketio = require('feathers-socketio');
+const feathers       = require('feathers');
+const configuration  = require('feathers-configuration');
+const hooks          = require('feathers-hooks');
+const rest           = require('feathers-rest');
+const socketio       = require('feathers-socketio');
 
-const handler = require('feathers-errors/handler');
-const notFound = require('feathers-errors/not-found');
+const handler        = require('feathers-errors/handler');
+const notFound       = require('feathers-errors/not-found');
 
-const middleware = require('./middleware');
-const base = require('./base');
-const services = require('./services');
-const appHooks = require('./app.hooks');
+const middleware     = require('./middleware');
+const base           = require('./base');
+const services       = require('./services');
+const appHooks       = require('./app.hooks');
 
 const authentication = require('./authentication');
 
-const mongoose = require('./mongoose');
+const mongoose       = require('./mongoose');
 
-const app = feathers();
+const app            = feathers();
 
 // Load app configuration
 app.configure(configuration());
